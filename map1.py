2617339c2bfed746f7546b7860e37765fd7e25fe
Ssykor <116069938+Ssykor@users.noreply.github.com>
2023-02-06 05:10:04 +0100

    Update map1.py

--- map1.py
+++ map1.py
@@ -2,43 +2,45 @@ import tkinter as tk
 import customtkinter
 import tkintermapview
 from geopy.distance import geodesic as GD
+import geocoder as gc
+from datetime import datetime as dt
+import json
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
+import re
 
 # customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 # customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-root = customtkinter.CTk() # tk.Tk()
+root = customtkinter.CTk()
 root.title("Pfadfinder")
 # root.iconbitmap("gui.ico")
 # root.geometry("1000x800")
-
-marker_count_path = 0
-marker_count_poi = 0
 
 color0 = "#333" # _darkgrey
 color1 = "#073330" # _darkgreen
 color2 ="#00bbff" #_lihtblue
 m_color = "#5533ff"
 
+switch_var = customtkinter.StringVar(value="off")
+
+marker_count_path = 0
+marker_count_poi = 0
+
+marker_list_path = []
+dist_list_path = []
+path_marker_list = []
+path_marker_dist_list = []
+new_marker_json_list = []
+search_results_json_list = []
+search_results = []
+
 search_frame = tk.Frame(root, width=1000, height=60, relief='groove', background=color1)
 main_frame = tk.Frame(root, width=1000, height=740, relief='groove', background=color0)
+infoFrame = tk.Frame(root, width=270, height=810, bg=color0, pady=0)
+btnFrame = tk.Frame(infoFrame, width=270, height=80, bg=color0, pady=10)
 
 # =================================================================================
-# search option menu and entry field
-# options_poi = ["Search by Address", "Search by Coordinates"]
-
-def get_search_option_poi(*args):
-    # print(f"search option has changed to '{option_clicked_poi.get()}'")
-    pass
-
-# option_clicked_poi = tk.StringVar(value='Search by Address')
-# option_clicked_poi.set(options_poi[0])
-# option_clicked_poi.trace('w', get_search_option_poi)
-
-# drop_poi = tk.OptionMenu(search_frame, option_clicked_poi, *options_poi)
-# drop_poi.config(bg=color1)
-# drop_poi.config(width=20)
-# drop_poi.config(height=1)
-# drop_poi.config(fg=color2)
 
 searchfield = tk.Entry(search_frame)
 searchfield.config(width=40)
@@ -47,18 +49,12 @@ searchfield.config(font=30)
 # ==============================================================================
 # Info Frame and Switch
 
-infoFrame = tk.Frame(root, width=270, height=810, bg=color0, pady=0)
-
-switch_var = customtkinter.StringVar(value="off")
-
 def info_switch_event():
     if switch_var.get() == "show data panel":
         main_frame.configure(width=1260)
         search_frame.configure(width=1260)
         infoSwitch.configure(text="hide data panel")
         infoFrame.place(relx=1, rely=0, anchor=tk.NE)
-        
-        # drop_poi.place(relx=0.009, rely=0.5, anchor=tk.W)
         searchfield.place(relx=0.143, rely=0.5, anchor=tk.W)
         btn_marker_poi.place(relx=0.4988, rely=0.51, anchor=tk.W)
         infoSwitch.place(relx=0.682, rely=0.51, anchor=tk.W)
@@ -67,8 +63,6 @@ def info_switch_event():
         main_frame.configure(width=1000)
         infoSwitch.configure(text="show data panel")
         infoFrame.place(relx=1, rely=0, anchor=tk.NW)
-        
-        # drop_poi.place(relx=0.0108, rely=0.5, anchor=tk.W)
         searchfield.place(relx=0.18, rely=0.5, anchor=tk.W)
         btn_marker_poi.place(relx=0.628, rely=0.51, anchor=tk.W)
         infoSwitch.place(relx=0.85, rely=0.51, anchor=tk.W)
@@ -78,12 +72,10 @@ def info_switch_event():
 infoSwitch = customtkinter.CTkSwitch(master=search_frame, text="show data panel", command=info_switch_event,
                                    variable=switch_var, onvalue="show data panel", offvalue="hide data panel", fg_color=("#fff", "#000"), text_color="#fff")
 
-# info_label_hello = tk.Label(infoFrame, width=25, height=1, text="", bg=color0, fg="#fff")
+info_label_poi = tk.Label(infoFrame, width=28, text="POIs (Places Of Interests)", font = ("Helvetica", 13), bg=color1, fg=color2, pady=19)
+listbox_marker_poi = tk.Listbox(infoFrame, selectmode=tk.MULTIPLE, height = 17, width=42, bd=None, borderwidth=0, relief=None,  bg=color0, font = ("Helvetica", 8), fg=color2)
 
-info_label_poi = tk.Label(infoFrame, width=28, text="Places Of Interests", font = ("Helvetica", 12), bg=color1, fg=color2, pady=19)
-listbox_marker_poi = tk.Listbox(infoFrame, height = 17, width=42, bd=0, bg=color0, font = ("Helvetica", 8), fg=color2)
-
-info_label_path = tk.Label(infoFrame, width=28, text="Path", font=("Helvetica", 12), bg="#5533ff", fg="#fff", pady=19)
+info_label_path = tk.Label(infoFrame, width=28, text="MARKERs on PATH", font=("Helvetica", 13), bg="#5533ff", fg="#fff", pady=19)
 marker_pos_label_path = tk.Label(infoFrame, width=36, text="Longitude, Latitude", bg="#fff", fg="#5533ff", pady=4)
 marker_dist_label_path = tk.Label(infoFrame, width=36, text="Distances", bg="#fff", fg="#5533ff", pady=4)
 
@@ -93,24 +85,80 @@ listbox_dist_path = tk.Listbox(infoFrame, height=10, width=42, bd=0, bg=color0, 
 # ==============================================================================
 # Button Functions
 
-search_results = []
+def convert_coordinates_to_address(deg_x: float, deg_y: float) -> gc.osm_reverse.OsmReverse:
+    """ returns address object with the following attributes:
+        street, housenumber, postal, city, state, country, latlng
+        Geocoder docs: https://geocoder.readthedocs.io/api.html#reverse-geocoding """
+    result = gc.osm([deg_x, deg_y], method="reverse")
 
-def change_search_map():
+    if result.ok:
+        return result.json
+
+
+def feed_map_poi_marker():
     global marker_count_poi
     marker_count_poi+=1
-    search_results.append(searchfield.get())
+
+    x = re.findall("[a-z]", searchfield.get())
+
     try:
-        listbox_marker_poi.insert(len(search_results)+1, "  POI "+str(len(search_results))+": "+searchfield.get())
-        map_widget.set_address(searchfield.get(), text="POI "+str(len(search_results)), marker=True, 
+        if len(x) > 0:
+            print(len(x))
+            search_results.append(searchfield.get())
+            search_results_json_list.append(searchfield.get())
+            listbox_marker_poi.insert(len(search_results)+1, "  P"+str(len(search_results))+": "+searchfield.get())
+        else:
+            print(len(x))
+            search_results.append(searchfield.get().replace(",", ""))
+            new_search_result_json = convert_coordinates_to_address((searchfield.get().replace(",", "").split(" ")[0]), (searchfield.get().replace(",", "").split(" ")[1]))
+            search_results_json_list.append(new_search_result_json)
+            listbox_marker_poi.insert(len(search_results)+1, "  P"+str(len(search_results))+": "+searchfield.get().replace(",", ""))
+
+        map_widget.set_address(searchfield.get(), text="P"+str(len(search_results)), marker=True, 
                                 marker_color_circle=color2, marker_color_outside=color1, text_color=color1)
+        print(search_results_json_list)
     except:
         print("not a valid entry")
 
+
+def feed_map_poi_marker_right_click(coords):
+    global marker_count_poi
+    marker_count_poi+=1
+
+    search_results.append(str(coords[0])+","+str(coords[1]))
+    new_search_result_json = convert_coordinates_to_address(str(coords[0]), str(coords[1]))
+    search_results_json_list.append(new_search_result_json)
+    listbox_marker_poi.insert(len(search_results)+1, "  P"+str(len(search_results))+": "+"{0:.7f}".format(coords[0])+" "+"{0:.7f}".format(coords[1]))
+
+    map_widget.set_marker(coords[0],coords[1], text="P"+str(marker_count_poi), 
+                                            marker_color_circle=color2, marker_color_outside=color1, text_color=color1)
+    print(search_results_json_list)
+
 # =============================================================================
-# Markers and Path
-def clear_map():
+# save & clear
+
+def save_all():
+    name = askstring('project name', 'name your project: ')
+    # showinfo('Hello!', 'Hi {}'.format(name))
+
+    dt_now = dt.now().strftime("%Y%m%d%H%M")
+
+    if len(name) == 0:
+        name = "noname"
+
+    poi_json_object = json.dumps(search_results_json_list, indent=4)
+    with open("C:/_data/__github/map/pois/pois_"+name+"_"+dt_now+".json", "w") as outfile:
+            outfile.write(poi_json_object)
+
+    path_marker_json_object = json.dumps(new_marker_json_list, indent=4)
+    with open("C:/_data/__github/map/path/path_"+name+"_"+dt_now+".json", "w") as outfile:
+            outfile.write(path_marker_json_object)
+
+
+def clear_all():
     global marker_count_path
     global marker_count_poi
+    
     map_widget.delete_all_marker()
     map_widget.delete_all_path()
     listbox_marker_poi.delete(0, marker_count_poi)
@@ -119,12 +167,12 @@ def clear_map():
     marker_count_poi = 0
     marker_count_path = 0
     search_results.clear()
+    search_results_json_list.clear()
     marker_list_path.clear()
     dist_list_path.clear()
-    print(search_results, marker_list_path, dist_list_path)
+    new_marker_json_list.clear()
 
-marker_list_path = []
-dist_list_path = []
+# =============================================================================
 
 def add_marker(coords):
     global marker_count_path
@@ -134,6 +182,10 @@ def add_marker(coords):
 
     marker_list_path.append(new_marker_path.position)
     listbox_marker_path.insert(len(marker_list_path)+1, "   M"+str(len(marker_list_path))+": "+str(new_marker_path.position[0])+", "+str(new_marker_path.position[1]))
+    path_marker_list.append((coords[0], coords[1]))
+    new_marker_json = convert_coordinates_to_address(coords[0], coords[1])
+    new_marker_json_list.append(new_marker_json)
+    print(new_marker_json_list)
 
     if len(marker_list_path) >= 2:
        penult =  marker_list_path[-2]
@@ -147,6 +199,16 @@ def add_marker(coords):
            sum += dist
 
        listbox_dist_path.insert(len(marker_list_path)+1, "   M"+str(marker_count_path-1)+"->"+"M"+str(marker_count_path)+": "+"{0:.3f}".format(single_dist_path)+" km"+"       "+"M1->M"+str(marker_count_path)+": "+"{0:.3f}".format(sum)+" km")
+       path_marker_dist_list.append("   M"+str(marker_count_path-1)+"->"+"M"+str(marker_count_path)+": "+"{0:.3f}".format(single_dist_path)+" km"+"       "+"M1->M"+str(marker_count_path)+": "+"{0:.3f}".format(sum)+" km")
+
+# =============================================================================
+
+def print_path_lists():
+    print('-'*100)
+    print(path_marker_list)
+    print('-'*100)
+    print(path_marker_dist_list)
+    print('-'*100)
 
 
 def func_help():
@@ -154,26 +216,25 @@ def func_help():
 
 # ==================================================================================================
 # Buttons
-btn_marker_poi = tk.Button(search_frame, text="SET POI", command=change_search_map, bg=color2, fg=color1)
+
+btn_marker_poi = tk.Button(search_frame, text="SHOW POI", command=feed_map_poi_marker, bg=color2, fg=color1)
 # btn_help = tk.Button(search_frame, text="  ?  ", bg="grey", fg="white", command=func_help)
 
-btn_clear = tk.Button(infoFrame, text="CLEAR", command=clear_map, bg="red", fg="white")
+btn_save_all = tk.Button(btnFrame, width=10, text="SAVE", command=save_all, bg="green", fg="white")
+btn_clear_all = tk.Button(btnFrame, width=10, text="CLEAR", command=clear_all, bg="red", fg="white")
 
 map_widget = tkintermapview.TkinterMapView(main_frame, width=1000, height=750, corner_radius=0)
-map_widget.add_right_click_menu_command(label="Add Marker", command=add_marker, pass_coords=True)
+map_widget.add_right_click_menu_command(label="Add PATH Marker", command=add_marker, pass_coords=True)
+map_widget.add_right_click_menu_command(label="Add POI Marker", command=feed_map_poi_marker_right_click, pass_coords=True)
 
 # ==================================================================================================
-# place all stuff
+# place
 
-# drop_poi.place(relx=0.0108, rely=0.5, anchor=tk.W)
 searchfield.place(relx=0.18, rely=0.5, anchor=tk.W)
 btn_marker_poi.place(relx=0.628, rely=0.51, anchor=tk.W)
 infoSwitch.place(relx=0.85, rely=0.51, anchor=tk.W)
-# btn_help.place(relx=0.98, rely=0.5, anchor=tk.E)
-
 map_widget.place(relx=0, rely=0.5, anchor=tk.W)
 
-# info_label_hello.pack()
 info_label_poi.pack()
 listbox_marker_poi.pack()
 info_label_path.pack()
@@ -181,11 +242,15 @@ marker_pos_label_path.pack()
 listbox_marker_path.pack()
 marker_dist_label_path.pack()
 listbox_dist_path.pack()
-btn_clear.pack() # place(relx=0.05, rely=0.05, anchor=tk.SE)
+btnFrame.pack()
+
+btn_save_all.grid(row=0, column=0)
+btn_clear_all.grid(row=0, column=1)
 
 search_frame.grid(row=0, column=0)
 main_frame.grid(row=1, column=0)
 
 # ==================================================================================================
 # play as long as you can (infinitly)
+
 root.mainloop()
